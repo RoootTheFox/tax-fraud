769f911474ae0107d9b1e9fcd703ae43e22593f1
rooot <hey@rooot.gay>
2024-05-27 20:20:29 +0200

    tax evasion >w<
    Signed-off-by: rooot <hey@rooot.gay>

--- Taxfraud-rs/src/main.rs
+++ Taxfraud-rs/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("\x1b[0;31mCommit Tax Fraud! Tax fraud good");
+    println!("\x1b[0;31mCommit Tax Fraud! Tax fraud good");
+    println!("\x1b[0;31mCommit Tax Fraud! Tax fraud good");
+    println!("\x1b[0;31mCommit Tax Fraud! Tax fraud good");
+
+    println!("catgirls support tax fraud !! ^-^");
 }
